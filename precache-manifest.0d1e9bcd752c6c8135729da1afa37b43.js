9273f070c8fa9ca5907ec840cdf53afa1825401a
havt <hai.v@shopstack.asia>
2019-03-13 11:54:50 +0700

    Updates

---
+++ precache-manifest.0d1e9bcd752c6c8135729da1afa37b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "989e9d75e3f72a261178fe06904238f3",
+    "url": "/reactjs-blog/static/media/blog_thumbnail.989e9d75.jpg"
+  },
+  {
+    "revision": "f042cfa99fe94646b3e9b97767d154b1",
+    "url": "/reactjs-blog/static/media/avartar_image.f042cfa9.png"
+  },
+  {
+    "revision": "fe854695ec16ec216ba1",
+    "url": "/reactjs-blog/static/js/runtime~main.1da3b764.js"
+  },
+  {
+    "revision": "5aa074ac1edfd9e20d7e",
+    "url": "/reactjs-blog/static/js/main.e54b60f7.chunk.js"
+  },
+  {
+    "revision": "2388e0ccf56ed49d950a",
+    "url": "/reactjs-blog/static/js/2.93bd3c0b.chunk.js"
+  },
+  {
+    "revision": "5aa074ac1edfd9e20d7e",
+    "url": "/reactjs-blog/static/css/main.2e44c42f.chunk.css"
+  },
+  {
+    "revision": "6ac50b6762776fc40311f6fe619fc9e5",
+    "url": "/reactjs-blog/index.html"
+  }
+];
